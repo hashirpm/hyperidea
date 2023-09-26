4144f6d39757a3b8891924f162f4e8669bb451d4
Hashir <mail2hashirpm@gmail.com>
2023-09-26 20:44:52 +0530

    ui fix

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Generate a&nbsp;</h1>
+        <h1 className={title()}>Generate your<br></br></h1>
         <h1 className={title({ color: "blue" })}>Hyper Idea&nbsp;</h1>
         <br />
         <h1 className={title()}>
--- components/IdeaCard.tsx
+++ components/IdeaCard.tsx
@@ -1,4 +1,14 @@
 import { Idea } from "@/model/Idea";
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image,
+} from "@nextui-org/react";
 
 type Props = {
   idea: Idea;
@@ -7,9 +17,28 @@ type Props = {
 function IdeaCard(props: Props) {
   return (
     <div>
-      <h1> {props.idea.title}</h1>
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <Image
+            alt="nextui logo"
+            height={40}
+            radius="sm"
+            src="/assets/hyperlane_logo.png"
+            width={40}
+          />
+          <div className="flex flex-col">
+            <p className="text-md">{props.idea.title}</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p>{props.idea.description}</p>
+        </CardBody>
+        <Divider />
+      </Card>
+      {/* <h1> {props.idea.title}</h1>
 
-      <p>{props.idea.description}</p>
+      <p>{props.idea.description}</p> */}
     </div>
   );
 }
