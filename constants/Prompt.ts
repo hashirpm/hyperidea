de2ea1544c9b157dc6873f63633441766e29964a
Hashir <mail2hashirpm@gmail.com>
2023-09-27 18:44:14 +0530

    prompt update

--- constants/Prompt.ts
+++ constants/Prompt.ts
@@ -1,10 +1,12 @@
 export const Prompt: string = `What I want you to consider is an idea for a project using Hyperlane. Hyperlane is the first universal and permissionless interoperability layer built for the modular blockchain stack. 
-Anyone can  to any blockchain environment, whether it is a layer 1, rollup, or app-chain, allowing that chain to communicate seamlessly with any other chain on which Hyperlane has been deployed.
-Hyperlane is designed with modularity in mind. Notably, its  gives developers control over their security model, allowing them to configure, compose, and customize security according to the needs of their application.
-Using Hyperlane, developers can build Interchain Applications - dapps that span multiple blockchains. Some pre-built examples that can be deployed out of the box include:
-1. Warp Routes which allow native, ERC20, and ERC721 tokens to move seamlessly across chains
+Anyone can  to any blockchain environment, whether it is a layer 1, rollup, or app-chain, allowing that chain to communicate seamlessly with any other chain on which Hyperlane has been deployed. 
+It  gives developers control over their security model, allowing them to configure, compose, and customize security according to the needs of their application.
+Using Hyperlane, developers can build Interchain Applications - dapps that span multiple blockchains. Some of the functionalities of Hyperlane are:
+1. Warp Routes which allow native, ERC20, and ERC721 tokens to move seamlessly across chains.  You can combine Warp Routes with a Hyperlane deployment to create economic trade routes between any chain and others already connected through Hyperlane.
 2. Interchain Accounts API , which allows an account on one chain (e.g. a DAO) to make smart contract calls on remote chains
-3. Interchain Queries API, which allow an account on one chain to make view calls on remote chains
+3. Interchain Queries API, which allow an account on one chain to make view calls on remote chains. 
+4. Messaging API,which provides an on-chain API for sending and receiving interchain messages.
+5. Liquidity Layer API, which wraps token bridges to allow developers to send tokens alongside their message.
 
     As much as possible, please make the project idea something that has never been seen before.
     Please output 2 project ideas. Each output should only include the project name and a brief description(about 150 words).
